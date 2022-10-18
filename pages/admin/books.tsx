5638e371efad53d402aa04bf4258a12fdbc81751
Eumatthew Cobalida <Letmego.MC@gmail.com>
2022-10-18 21:46:04 +0100

    added modal for lending books

--- pages/admin/books.tsx
+++ pages/admin/books.tsx
@@ -1,17 +1,54 @@
-import { Box, Button, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { FC, useEffect, useState } from 'react';
 
 const Books: FC = () => {
   const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const load = async () => {
+  const getBooks = async () => {
     const data = await fetch('http://localhost:3002/books');
     const response = await data.json();
     setData(response);
   };
 
+  const getUsers = async () => {
+    const data = await fetch('http://localhost:3002/users');
+    const response = await data.json();
+    setUsers(response.data);
+  };
+
+  const openModal = (quantity) => {
+    if (quantity) {
+      onOpen();
+    }
+  };
+
   useEffect(() => {
-    load();
+    getBooks();
+    getUsers();
   }, []);
 
   return (
@@ -47,7 +84,12 @@ const Books: FC = () => {
                   <Td>{book.isbn}</Td>
                   <Td>{book.quantity}</Td>
                   <Td>
-                    <Button colorScheme="cyan" color="white">
+                    <Button
+                      disabled={!book.quantity}
+                      colorScheme="cyan"
+                      color="white"
+                      onClick={() => openModal(book.quantity)}
+                    >
                       Lend
                     </Button>
                   </Td>
@@ -57,6 +99,35 @@ const Books: FC = () => {
           </Tbody>
         </Table>
       </TableContainer>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Lend Book</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Student</FormLabel>
+              <Select placeholder="Select student">
+                {users.map((user: any) => {
+                  return (
+                    <option>
+                      {user.first_name} {user.last_name}
+                    </option>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button colorScheme="teal">Lend</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
